4034b8f7926b0747d3664e2a835b884ea069d039
Scott <scottgeiser@outlook.com>
2021-08-05 14:18:03 -0700

    add netlify folder

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+        <div className="App">
+        <header className="App-header">
+            <p></p>
+        </header>
+        </div>
+            <p>
+                About Page
+            </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <React.Fragment>
+        <div className="App">
+        <header className="App-header">
+            <p>nav goes here</p>
+        </header>
+        </div>
+            <p>
+                SCOTT HIKES Home Page.
+            </p>
+    </React.Fragment>
+  );
+}
+
+export default Home;
